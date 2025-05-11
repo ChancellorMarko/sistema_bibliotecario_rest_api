b00615bc87976168254002c698bae885ad34bc43
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-05-11 15:42:01 -0300

    Classe Livro criada

--- api_biblioteca/demo/src/main/java/com/example/demo/Entities/Livro.java
+++ api_biblioteca/demo/src/main/java/com/example/demo/Entities/Livro.java
@@ -1,2 +1,42 @@
 package com.example.demo.Entities;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "livros")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String autor;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    @Column(nullable = false)
+    private String categoria;
+
+}
