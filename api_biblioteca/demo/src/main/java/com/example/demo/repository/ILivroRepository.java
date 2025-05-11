0df92efb85381d115bc019e648e4009662b1ee37
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-05-11 15:48:25 -0300

    repository Livro

---
+++ api_biblioteca/demo/src/main/java/com/example/demo/repository/ILivroRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.Entities.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ILivroRepository extends JpaRepository<Livro, Long> {
+}
+
+
+    
