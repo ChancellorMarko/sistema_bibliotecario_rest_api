87d055d492288434ae584eb5321cff537460d987
Pedro-Lima17 <168600996+Pedro-Lima17@users.noreply.github.com>
2025-05-15 00:28:45 -0300

    implementação de DTO e Mapper para Cliente e  atualização do controller

---
+++ api_biblioteca/demo/src/main/java/com/example/demo/dto/ClienteDTO.java
@@ -0,0 +1,29 @@
+package com.example.demo.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ClienteDTO {
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
+    private String email;
+
+    @NotBlank(message = "O telefone é obrigatório")
+    private String telefone;
+
+    @NotBlank(message = "O endereço é obrigatório")
+    private String endereco;
+}
+
