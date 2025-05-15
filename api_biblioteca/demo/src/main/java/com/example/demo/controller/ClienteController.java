92e5b45be9e3838b62a8f3cbca6b364e85e2ba0f
Pedro-Lima17 <168600996+Pedro-Lima17@users.noreply.github.com>
2025-05-14 23:27:44 -0300

    Remoção completa da entidade Usuario e ajustes no Swagger do Cliente

--- api_biblioteca/demo/src/main/java/com/example/demo/controller/ClienteController.java
+++ api_biblioteca/demo/src/main/java/com/example/demo/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.example.demo.Entities.Cliente;
 import com.example.demo.service.ClienteService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 //import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "Cliente", description = "Endpoints para gerenciamento de clientes")
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
