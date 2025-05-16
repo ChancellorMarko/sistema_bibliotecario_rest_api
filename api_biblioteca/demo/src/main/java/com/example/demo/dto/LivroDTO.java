a0d966db0ae1114945cf5968c190ceb6b8eea0af
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-05-16 20:02:21 -0300

    DTO

---
+++ api_biblioteca/demo/src/main/java/com/example/demo/dto/LivroDTO.java
@@ -0,0 +1,32 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LivroDTO {
+    private Long id;
+
+    @NotBlank(message = "O título é obrigatório")
+    @Size(max = 100, message = "O título deve ter no máximo 100 caracteres")
+    private String titulo;
+
+    @NotBlank(message = "O autor é obrigatório")
+    @Size(max = 100, message = "O autor deve ter no máximo 100 caracteres")
+    private String autor;
+
+    @NotBlank(message = "O ISBN é obrigatório")
+    @Size(max = 20, message = "O ISBN deve ter no máximo 20 caracteres")
+    private String isbn;
+
+    @NotNull(message = "A quantidade é obrigatória")
+    private Integer quantidade;
+
+    @NotBlank(message = "A categoria é obrigatória")
+    @Size(max = 50, message = "A categoria deve ter no máximo 50 caracteres")
+    private String categoria;
+}
