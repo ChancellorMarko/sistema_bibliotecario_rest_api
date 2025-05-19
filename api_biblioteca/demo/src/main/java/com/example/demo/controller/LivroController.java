25b474fd40d451f0aa8fbfba9e2c8a491c2b5ca2
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-05-19 20:26:41 -0300

    k

--- api_biblioteca/demo/src/main/java/com/example/demo/controller/LivroController.java
+++ api_biblioteca/demo/src/main/java/com/example/demo/controller/LivroController.java
@@ -1,3 +1,59 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.LivroDTO;
+import com.example.demo.service.LivroService;
+import com.example.demo.mapper.LivroMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/livros")
+public class LivroController {
+
+    @Autowired
+    private LivroService livroService;
+
+    @Autowired
+    private LivroMapper livroMapper;
+
+     @PostMapping
+    public ResponseEntity<LivroDTO> criarLivro(@RequestBody LivroDTO livroDTO) {
+        var livro = livroMapper.toEntity(livroDTO);
+        var livroSalvo = livroService.salvar(livro);
+        return ResponseEntity.ok(livroMapper.toDTO(livroSalvo));
+    }
+
+    // Listar todos os livros
+    @GetMapping
+    public ResponseEntity<List<LivroDTO>> listarLivros() {
+        var livros = livroService.buscarTodos();
+        return ResponseEntity.ok(livroMapper.toDTOList(livros));
+    }
+
+    // Buscar por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<LivroDTO> buscarPorId(@PathVariable Long id) {
+        var livro = livroService.buscarPorId(id);
+        return ResponseEntity.ok(livroMapper.toDTO(livro));
+    }
+
+    // Atualizar livro
+    @PutMapping("/{id}")
+    public ResponseEntity<LivroDTO> atualizar(@PathVariable Long id, @RequestBody LivroDTO livroDTO) {
+        var livro = livroMapper.toEntity(livroDTO);
+        var atualizado = livroService.atualizar(id, livro);
+        return ResponseEntity.ok(livroMapper.toDTO(atualizado));
+    }
+
+    // Deletar livro
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        livroService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
 
--- api_biblioteca/demo/target/generated-sources/annotations/com/example/demo/mapper/LivroMapperImpl.java
+++ api_biblioteca/demo/target/generated-sources/annotations/com/example/demo/mapper/LivroMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-16T21:36:20-0300",
+    date = "2025-05-19T20:17:41-0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.v20250514-1000, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
