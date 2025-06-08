1e9e91122d32366bf49db3f06e9383ea76252e22
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-06-08 16:52:53 -0300

    Adicionando a classe de manutenção

---
+++ api_biblioteca/demo/src/main/java/com/example/demo/Entities/Manutencao.java
@@ -0,0 +1,38 @@
+package com.example.demo.Entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "manutencoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Manutencao {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+private String descricao;
+
+
+private String status;
+
+
+private LocalDateTime dataInicio;
+
+
+private LocalDateTime dataFim;
+
+
+@ManyToOne
+@JoinColumn(name = "livro_id", nullable = false)
+private Livro livro;
+}
