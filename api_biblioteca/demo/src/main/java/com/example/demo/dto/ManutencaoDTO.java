042df299d1c293ec94aecbdb5d579051de9c92eb
Lizzandra Alves <168601460+Chiquinhosorvetes@users.noreply.github.com>
2025-06-08 16:57:41 -0300

    Manutenção subindo, olha a cabeça

---
+++ api_biblioteca/demo/src/main/java/com/example/demo/dto/ManutencaoDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.dto;
+
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+
+import java.time.LocalDateTime;
+
+
+@Data
+public class ManutencaoDTO {
+private Long id;
+
+
+@NotBlank(message = "A descrição é obrigatória")
+private String descricao;
+
+
+@NotBlank(message = "O status da manutenção é obrigatório")
+private String status;
+
+
+@NotNull(message = "A data de início é obrigatória")
+private LocalDateTime dataInicio;
+
+
+private LocalDateTime dataFim;
+
+
+@NotNull(message = "O ID do livro é obrigatório")
+private Long livroId;
+}
+
